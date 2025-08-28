2218da31bcdc72907fec7ed66758c64b406d8ef0
Prabhakaran A K <145427141+Prabha-Uchiha@users.noreply.github.com>
2025-08-28 21:05:29 +0530

    Create signofInput.java

---
+++ mod2/signofInput.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        
+        if(n==0){
+            System.out.println("Zero");}
+        
+        else if (n>0)
+            System.out.println("Positive");
+        
+        else
+            System.out.println("Negative");
+    }
+}
